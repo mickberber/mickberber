0b9f7f02c1e6b9bd7f59abc3865a136eaaa6b34c
Michael Berber <mickberber@icloud.com>
2016-07-03 22:09:39 -0700

    add to about me

--- dist/bundle.js
+++ dist/bundle.js
@@ -41026,7 +41026,86 @@
 	        'div',
 	        null,
 	        _react2.default.createElement(_nav2.default, { textColor: 'black' }),
-	        _react2.default.createElement('img', { src: './../../assets/IMG_0925.JPG' })
+	        _react2.default.createElement('div', { style: { height: '100px' } }),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'row' },
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginLeft: '25px' } },
+	            'I have a really cool dog'
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginRight: '25px', float: 'right' } },
+	            'And I cook a little bit...'
+	          )
+	        ),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px' }, src: './../../assets/IMG_0925.JPG' }),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px', float: 'right' }, src: './../../assets/food.JPG' }),
+	        _react2.default.createElement('div', { style: { height: '100px' } }),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'row' },
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginLeft: '25px' } },
+	            'I\'m a red'
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginRight: '25px', float: 'right' } },
+	            'YNWA'
+	          )
+	        ),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px' }, src: './../../assets/lfc.jpg' }),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px', float: 'right' }, src: './../../assets/lfc2.JPG' }),
+	        _react2.default.createElement('div', { style: { height: '100px' } }),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'row' },
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginLeft: '25px' } },
+	            'The GOAT, my all time favorite'
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginRight: '25px', float: 'right' } },
+	            'I\'m green and gold'
+	          )
+	        ),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px' }, src: './../../assets/HENDERSON.JPG' }),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px', float: 'right' }, src: './../../assets/oak.JPG' }),
+	        _react2.default.createElement('div', { style: { height: '100px' } }),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'row' },
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginLeft: '25px' } },
+	            'Black and Blue in my veins'
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginRight: '25px', float: 'right' } },
+	            'Mmmm... a comeback'
+	          )
+	        ),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px' }, src: './../../assets/avaya.JPG' }),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px', float: 'right' }, src: './../../assets/gordon.JPG' }),
+	        _react2.default.createElement('div', { style: { height: '100px' } }),
+	        _react2.default.createElement(
+	          'div',
+	          { className: 'row' },
+	          _react2.default.createElement(
+	            'div',
+	            { style: { marginLeft: '25px' } },
+	            'And I take some decent photos'
+	          )
+	        ),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px' }, src: './../../assets/palace.JPG' }),
+	        _react2.default.createElement('img', { style: { height: '500px', width: '50%', padding: '20px', float: 'right' }, src: './../../assets/sf.JPG' })
 	      );
 	    }
 	  }]);
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,7 +8,40 @@ export default class About extends Component {
     return (
       <div>
         <Nav textColor='black' />
-        <img src='./../../assets/IMG_0925.JPG'/>
+        <div style={{height: '100px'}}/>
+        <div className='row'>
+          <div style={{marginLeft:'25px'}}>I have a really cool dog</div>
+          <div style={{marginRight: '25px', float:'right'}}>And I cook a little bit...</div>
+        </div>
+        <img style={{height: '500px', width: '50%', padding: '20px'}} src='./../../assets/IMG_0925.JPG'/>
+        <img style={{height: '500px', width: '50%', padding: '20px', float: 'right'}} src='./../../assets/food.JPG'/>
+        <div style={{height: '100px'}}/>
+        <div className='row'>
+          <div style={{marginLeft:'25px'}}>I'm a red</div>
+          <div style={{marginRight: '25px', float:'right'}}>YNWA</div>
+        </div>
+        <img style={{height: '500px', width: '50%', padding: '20px'}} src='./../../assets/lfc.jpg'/>
+        <img style={{height: '500px', width: '50%', padding: '20px', float: 'right'}} src='./../../assets/lfc2.JPG'/>
+        <div style={{height: '100px'}}/>
+        <div className='row'>
+          <div style={{marginLeft:'25px'}}>The GOAT, my all time favorite</div>
+          <div style={{marginRight: '25px', float:'right'}}>I'm green and gold</div>
+        </div>
+        <img style={{height: '500px', width: '50%', padding: '20px'}} src='./../../assets/HENDERSON.JPG'/>
+        <img style={{height: '500px', width: '50%', padding: '20px', float: 'right'}} src='./../../assets/oak.JPG'/>
+        <div style={{height: '100px'}}/>
+        <div className='row'>
+          <div style={{marginLeft:'25px'}}>Black and Blue in my veins</div>
+          <div style={{marginRight: '25px', float:'right'}}>Mmmm... a comeback</div>
+        </div>
+        <img style={{height: '500px', width: '50%', padding: '20px'}} src='./../../assets/avaya.JPG'/>
+        <img style={{height: '500px', width: '50%', padding: '20px', float: 'right'}} src='./../../assets/gordon.JPG'/>
+        <div style={{height: '100px'}}/>
+        <div className='row'>
+          <div style={{marginLeft:'25px'}}>And I take some decent photos</div>
+        </div>
+        <img style={{height: '500px', width: '50%', padding: '20px'}} src='./../../assets/palace.JPG'/>
+        <img style={{height: '500px', width: '50%', padding: '20px', float: 'right'}} src='./../../assets/sf.JPG'/>
       </div>
     );
   }
